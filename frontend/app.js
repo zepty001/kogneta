06a11c80aaf2bdf56b69f8461670996a9ae9185f
zepty001 <108428950+zepty001@users.noreply.github.com>
2024-06-22 20:38:19 +0530

    Create app.js

---
+++ frontend/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+// const PORT = process.env.PORT || 3000;
+const PORT = 3000;
+
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
